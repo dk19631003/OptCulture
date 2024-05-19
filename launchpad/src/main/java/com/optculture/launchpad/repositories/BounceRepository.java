0f542124ac0be238d82ddebc3549b8e94f4a999c
dinesh <dinesh@optculture.com>
2024-05-19 21:09:38 +0530

    launchpad removed

--- launchpad/src/main/java/com/optculture/launchpad/repositories/BounceRepository.java
+++
@@ -1,9 +0,0 @@
-package com.optculture.launchpad.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.optculture.shared.entities.communication.email.Bounces;
-
-public interface BounceRepository extends JpaRepository<Bounces,Long> {
-
-}
